bf9481b52dd66b9dd30af783790e4f8bfdff4116
ArshiaRahmany <rahmanyarshia@gmail.com>
2024-02-16 02:29:12 +0330

    landing page complete

--- src/components/searchBackground.vue
+++ src/components/searchBackground.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        <img src="https://s3-alpha-sig.figma.com/img/4766/5c78/439990f32f84fdc928a383f332530b8a?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Lsgf-BxVC9ejcvFKeftWh2vIOlRgPes1wI7UPeO9AciuZjy6SRzTAveXGKhawGEcTRAtinE4Z2npgruU1tIwb1Q3bXo2IYFSkS-uDkYikwfYDY7zLJukLprOYb3H2eV9sBLekqRPhjgSsNalyQWLruK2vMeJB1oLQLUGCKyZV6NeY7~IoKH9s2h5T4KI1SwUhJwJAmtn9jsiKx0jeFso5043yMBjUfxylfCAwSYuS~2iQjxg632o3CDTyxikKN7Ozt-x0xVQQMGAM9Qr7QsmZZgsJAZtTgL6x79WgY4H7RVGo-19Fe4~BoHIcC9A1txmtv~OQ~Xi8jvXFyrvKhX0DQ__"
-            alt="">
+    <div class="container">
+        <form class="searchContainer">
+            <input type="text" placeholder="&#xF002; Where are you going?">
+            <button>Search</button>
+        </form>
     </div>
 </template>
 
@@ -26,12 +28,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-div{
-    margin-top: 100px;
-    text-align: center;
-}
-img {
+.container {
     width: 100%;
     height: 440px;
+    background: url("https://s3-alpha-sig.figma.com/img/4766/5c78/439990f32f84fdc928a383f332530b8a?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Lsgf-BxVC9ejcvFKeftWh2vIOlRgPes1wI7UPeO9AciuZjy6SRzTAveXGKhawGEcTRAtinE4Z2npgruU1tIwb1Q3bXo2IYFSkS-uDkYikwfYDY7zLJukLprOYb3H2eV9sBLekqRPhjgSsNalyQWLruK2vMeJB1oLQLUGCKyZV6NeY7~IoKH9s2h5T4KI1SwUhJwJAmtn9jsiKx0jeFso5043yMBjUfxylfCAwSYuS~2iQjxg632o3CDTyxikKN7Ozt-x0xVQQMGAM9Qr7QsmZZgsJAZtTgL6x79WgY4H7RVGo-19Fe4~BoHIcC9A1txmtv~OQ~Xi8jvXFyrvKhX0DQ__");
+    background-size: 100% 440px;
+    text-align: center;
+}
+
+form {
+    position: relative;
+    left: 6%;
+    top: 147px;
+    width: 80%;
+    height: 86px;
+    margin: auto;
+    border-radius: 40px;
+    display: flex;
+}
+
+input {
+    border: 1px;
+    border: 1px solid #EDF2F4;
+    box-shadow: 0px 1px 5px 0px #00000059;
+    background: linear-gradient(0deg, #EDF2F4, #EDF2F4),
+        inear-gradient(0deg, #FFFFFF, #FFFFFF);
+    width: 100%;
+    padding: 16px 20px 16px 20px;
+    border-radius: 40px;
+    border: 1px;
+    outline: none;
+    transform: translateY(-2.45%);
+}
+
+button {
+    background: #F0284D;
+    width: 241.92px;
+    height: 79.86px;
+    border-radius: 30px;
+    color: white;
+    border: none;
+    font-family: Inter;
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 23px;
+    letter-spacing: 0em;
+    cursor: pointer;
+    transform: translateX(-102.5%);
 }
 </style>
--- src/components/tourCards.vue
+++ src/components/tourCards.vue
@@ -1,15 +1,20 @@
 <template>
-    <div>
+    <div class="container">
         <div class="theCard">
             <div class="cover">
                 <img src="" alt="">
             </div>
-            <div class="aboutTour">
-                <p> {{ location }} </p>
-                <p class="description"> {{ descriptions }} </p>
-            </div>
-            <div class="date">
-                {{ date }}
+            <div class="txtHolder">
+                <div class="aboutTour">
+                    <p id="destination"> Dubai </p>
+                    <p id="description"> Dubai Miracle Garden Skip-the-Line Entry Tickets</p>
+                </div>
+                <div id="inception">
+                    from
+                    <span>
+                        895.00 AED
+                    </span>
+                </div>
             </div>
         </div>
     </div>
@@ -21,20 +26,73 @@ export default {
 
     data() {
         return {
-            
+
         };
     },
 
     mounted() {
-        
+
     },
 
     methods: {
-        
+
     },
 };
 </script>
 
 <style lang="scss" scoped>
+.container {
+    margin-bottom: 40px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
 
-</style>
+.theCard {
+    height: 448px;
+    width: 262px;
+    padding: 0px, 0px, 10px, 0px;
+    border-radius: 10px;
+    gap: 12px;
+    border: 1px solid #ADB3CC;
+}
+
+.cover img {
+    width: 100%;
+    height: 280px;
+    border-radius: 10px 10px 0px 0px;
+}
+
+.txtHolder {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-inline: 4%;
+}
+
+#destination,
+#inception {
+    font-family: Inter;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 17px;
+    letter-spacing: 0em;
+    color: #8590A4;
+    margin-bottom: 0px;
+}
+
+#description,
+span {
+    margin-top: 0px;
+    font-family: Inter;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 22px;
+    letter-spacing: 0em;
+    color: #2B2D42;
+}
+
+#inception {
+    position: relative;
+    top: 40px;
+}</style>
